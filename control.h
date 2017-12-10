021db275e1860a298efadeba57ac7d81478d2f3b
TakingTheL <yliu6@stuy.edu>
2017-12-10 14:11:12 -0500

    finished control.h with all necessary includes

--- control.h
+++ control.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <string.h>
+
 #ifndef CONTROL
 #define CONTROL
 
